48928b8a59ab9384a9740277db2c4e9ab11f0e72
Ali <alibaniasad1999@yahoo.com>
2020-08-04 21:25:38 +0430

    demi final

--- main2.py
+++ main2.py
@@ -11,7 +11,7 @@ h_air = 4.5 / 1000000
 h_water = 890 / 1000000
 T_air = 30
 T_water = 20
-w = 0.01 / 1000
+w = 0.1 / 1000
 
 
 # r function from x y z
@@ -51,6 +51,7 @@ for i in range(201):
     for j in range(101):
         a[i, j] = 30
 n = int(input())
+f = open("final center for 1000 s .txt", "w")
 for k in range(n):
     for i in range(1, 200):
         for j in range(101):
@@ -61,30 +62,34 @@ for k in range(n):
                                    area_down(index2z(j)) / r + 2 * area_side_get(index2z(j)) / l))
             elif j == 100:
                 a[i][j] = (area_up(index2z(j)) * h_air * T_air + K * (a[i][j - 1] * (
-                    area_down(index2z(j))) / r + area_side_get(index2z(j)) / l) * (a[i - 1][j] + a[i + 1][j])) / (
-                                  area_up(index2z(j)) * h_water + K * (
+                    area_down(index2z(j))) / r + area_side_get(index2z(j)) / l * (a[i - 1][j] + a[i + 1][j]))) / (
+                                  area_up(index2z(j)) * h_air + K * (
                                    area_down(index2z(j)) / r + 2 * area_side_get(index2z(j)) / l))
             elif 0 < j < 51:
                 a[i][j] = (K / r * (area_down(index2z(j)) * a[i][j + 1] + area_up(index2z(j)) * a[i][j - 1]) + K / l * (
                         area_side_get(index2z(j))) * (a[i - 1][j] + a[i + 1][j])) / (
                                   K / r * (area_down(index2z(j)) + area_up(index2z(j))) + K / l * 2 * area_side_get(
-                                index2z(j)))
+                                 index2z(j)))
             else:
                 a[i][j] = (K / r * (area_down(index2z(j)) * a[i][j - 1] + area_up(index2z(j)) * a[i][j + 1]) + K / l * (
                         area_side_get(index2z(j))) * (a[i - 1][j] + a[i + 1][j])) / (
                                   K / r * (area_down(index2z(j)) + area_up(index2z(j))) + K / l * 2 * area_side_get(
-                              index2z(j)))
+                               index2z(j)))
+            # f.write(str(int((1000*a[i][j]))/1000))
+            # f.write(' ')
+        # f.write('///////////////')
+
     i = 200
     for j in range(101):
         if j == 0:
-            a[i][j] = (area_up(index2z(j)) * h_water * T_water + a[i][j + 1] * K * (a[i][j+1] * (
-                    area_down(index2z(j))) / r + area_side_get(index2z(j)) / l * (a[i - 1][j])) + w * area_side_get(index2z(j))) / (
-                              area_up(index2z(j)) * h_water + K * (
+            a[i][j] = (area_up(index2z(j)) * h_water * T_water + K * (a[i][j+1] * (
+                    area_down(index2z(j))) / r + area_side_get(index2z(j)) / l * (a[i - 1][j])) + w * area_side_get(
+                index2z(j))) / (area_up(index2z(j)) * h_water + K * (
                                area_down(index2z(j)) / r + area_side_get(index2z(j)) / l))
         elif j == 100:
-            a[i][j] = (area_up(index2z(j)) * h_air * T_air + a[i][j - 1] * K * (a[i][j-1] * (
-                    area_down(index2z(j))) / r + area_side_get(index2z(j)) / l * (a[i - 1][j])) + w * area_side_get(index2z(j))) / (
-                              area_up(index2z(j)) * h_water + K * (
+            a[i][j] = (area_up(index2z(j)) * h_air * T_air + K * (a[i][j-1] * (
+                    area_down(index2z(j))) / r + area_side_get(index2z(j)) / l * (a[i - 1][j])) + w * area_side_get(
+                 index2z(j))) / (area_up(index2z(j)) * h_air + K * (
                                area_down(index2z(j)) / r + area_side_get(index2z(j)) / l))
         elif 0 < j < 51:
             a[i][j] = (K / r * (area_down(index2z(j)) * a[i][j + 1] + area_up(index2z(j)) * a[i][j - 1]) + K / l * (
@@ -94,6 +99,16 @@ for k in range(n):
             a[i][j] = (K / r * (area_down(index2z(j)) * a[i][j - 1] + area_up(index2z(j)) * a[i][j + 1]) + K / l * (
                     area_side_get(index2z(j))) * (a[i - 1][j]) + area_side_get(index2z(j)) * w) / (
                               K / r * (area_down(index2z(j)) + area_up(index2z(j))) + K / l * area_side_get(index2z(j)))
-f = open("final senter.txt", "w")
+        # f.write(str(int((1000*a[i][j]))/1000))
+    # f.write('///////////////')
+    # f.write('@@@@@@@@@@@')
+    # print(a[199][100], a[200][100])
+# for i in range(201):
+#     print(i, (a[i][50]))
+# for j in range(101):
+#     print(j, (a[0][j]))
 for i in range(201):
-    print(i, (a[i][50]))
+    for j in range(101):
+        f.write(str(int((1000*a[i][j]))/1000))
+        f.write(' ')
+    f.write('\n')
